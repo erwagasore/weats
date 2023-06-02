6f672224262612189af3d409e272cb42da948d2c
Eugene Rwagasore <rw-g-s-r-@er.local>
2023-06-02 22:20:27 +0200

    Initialized the Rust project

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use core::fmt;
+
+struct Greeting {
+    name: String,
+}
+
+impl Greeting {
+    fn new<T: AsRef<str>>(name: T) -> Self {
+        Greeting {
+            name: name.as_ref().to_string(),
+        }
+    }
+}
+
+impl fmt::Display for Greeting {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Hello, {}!", self.name)
+    }
+}
+
+fn main() {
+    let greeting = Greeting::new("Kigali");
+    println!("Hello, {}!", greeting);
+}
